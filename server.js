73043052e8b94c008850e3802f7023bf4506ccdb
WidAlsa0 <69405330+WidAlsa0@users.noreply.github.com>
2022-01-26 12:47:14 -0500

    created different exit code

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ fs.readFile("./www/index.html", "utf8", (err, data) => {
 if (err) {
   console.error(err)
   return
-  process.exit(1)
+  process.exit(7)
+} else {
+  null
 }
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
